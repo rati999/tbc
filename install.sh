1851e25fa13a9e46e1590b60d1c8bc821283df31
rati999 <93841566+rati999@users.noreply.github.com>
2024-07-26 05:23:43 +0400

    install.sh

--- install.sh
+++ install.sh
@@ -1,52 +1,52 @@
 #!/bin/bash
 set -e
-
+ 
 # Update package repository
 sudo apt-get update
-
+ 
 # Install prerequisites
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-
+ 
 # Add Docker's official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
+ 
 # Set up the Docker repository
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
+ 
 # Update package repository again
 sudo apt-get update
-
+ 
 # Install Docker
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-
+ 
 # Enable and start Docker service
 sudo systemctl enable docker
 sudo systemctl start docker
-
+ 
 # Create Docker network
 docker network create monitoring
-
+ 
 # Install and configure Prometheus
 mkdir -p /opt/prometheus
 cat << EOF > /opt/prometheus/prometheus.yml
 global:
   scrape_interval: 15s
-
+ 
 scrape_configs:
   - job_name: 'node'
     static_configs:
       - targets: ['node-exporter:9100']
-
+ 
 rule_files:
   - 'alert.rules'
-
+ 
 alerting:
   alertmanagers:
   - static_configs:
     - targets:
       - 'alertmanager:9093'
 EOF
-
+ 
 cat << EOF > /opt/prometheus/alert.rules
 groups:
 - name: example
@@ -60,14 +60,14 @@ groups:
       summary: High CPU usage detected
       description: CPU usage is above 10% for 5 minutes
 EOF
-
+ 
 docker run -d \
     --name prometheus \
     --network monitoring \
     -p 9090:9090 \
     -v /opt/prometheus:/etc/prometheus \
     prom/prometheus
-
+ 
 # Install and configure Alertmanager
 mkdir -p /opt/alertmanager
 cat << EOF > /opt/alertmanager/config.yml
@@ -82,14 +82,14 @@ receivers:
   webhook_configs:
   - url: 'http://127.0.0.1:5001/'
 EOF
-
+ 
 docker run -d \
     --name alertmanager \
     --network monitoring \
     -p 9093:9093 \
     -v /opt/alertmanager:/etc/alertmanager \
     prom/alertmanager
-
+ 
 # Install Node Exporter
 docker run -d \
     --name node-exporter \
@@ -98,15 +98,16 @@ docker run -d \
     -v "/:/host:ro,rslave" \
     quay.io/prometheus/node-exporter \
     --path.rootfs=/host
-
+ 
 # Install WordPress
 docker run -d -p 80:80 \
   --name wordpress \
   --network monitoring \
-  -e WORDPRESS_DB_HOST=terraform-20240725221607873200000001.clcyocousbmq.eu-central-1.rds.amazonaws.com \
+  -e WORDPRESS_DB_HOST=terraform-20240726011257722100000002.clcyocousbmq.eu-central-1.rds.amazonaws.com \
   -e WORDPRESS_DB_USER=admin \
   -e WORDPRESS_DB_PASSWORD=ratirati \
   -e WORDPRESS_DB_NAME=wordpress_db \
   wordpress
-
+ 
 echo "Installation completed successfully!"
+ 
